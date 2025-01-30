13c86f704a5131653feb2d18dd9b2a6245f3d4ce
Hasan <hasan0407@gmail.com>
2025-01-30 07:43:07 -0800

    Done with basic models user, serviceprovider, jobseeker

--- backEnd/app/models.py
+++ backEnd/app/models.py
@@ -4,80 +4,86 @@ from config import db
 from sqlalchemy import DateTime
 from sqlalchemy.dialects.postgresql import ENUM
 
-GENDER_ENUM = ENUM(
-    'male',
-    'female',
-    name='gender_type'
-)
+GENDER_ENUM = ENUM("male", "female", name="gender_type")
+SERVICE_TYPE_ENUM = ENUM("peon", "maid", "driver", name="service_type")
 
-SERVICE_TYPE_ENUM = ENUM(
-    'peon',
-    'maid',
-    'driver',
-    name='service_type'
-)
 
-class ServiceProvider(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False, unique=True)
-    user = db.relationship('User', backref='service_provider', uselist=False)
-    serviceType = db.Column(SERVICE_TYPE_ENUM, unique=False, nullable=False)
-    numberOfReviewsReceived = db.Column(db.Integer, default=0)
+class ServiceProvider(User):
+    __tablename__ = "service_providers"
+
+    id = db.Column(db.Integer, db.ForeignKey("users.id"), primary_key=True)
+    service_provided = db.Column(SERVICE_TYPE_ENUM, nullable=False)
+    number_of_reviews_received = db.Column(db.Integer, default=0)
     distance = db.Column(db.Float, default=0.0)
     rating = db.Column(db.Float, default=0.0)
+    rate = db.Column(db.Float, default=0.0)
+
+    __mapper_args__ = {"polymorphic_identity": "service_provider"}
+
+    def to_json(self):
+        base = super().to_json()
+        base.update(
+            {
+                "service_provided": self.service_provided,
+                "number_of_reviews_received": self.number_of_reviews_received,
+                "distance": self.distance,
+                "rating": self.rating,
+                "rate": self.rate,
+            }
+        )
+
+        return base
+
+
+class JobSeeker(User):
+    __tablename__ = "job_seekers"
+
+    id = db.Column(db.Integer, db.ForeignKey("users.id"), primary_key=True)
+    service_type = db.Column(SERVICE_TYPE_ENUM, nullable=False)
+
+    __mapper_args__ = {"polymorphic_identity": "job_seeker"}
+
+    def to_json(self):
+        base = super().to_json()
+        base.update({"service_type": self.service_type})
+
+        return base
+
+
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(80), nullable=False)
+    middle_name = db.Column(db.String(80), nullable=True)
+    last_name = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(120), nullable=True)
+    house_address = db.Column(db.String(225), nullable=False)
+    date_of_birth = db.Column(DateTime, nullable=False)
+    phone_number = db.Column(db.String(15), nullable=False)
+    profile_picture = db.Column(db.String(225), nullable=True)
+    gender = db.Column(GENDER_ENUM, nullable=False)
+    created_at = db.Column(DateTime, default=datetime.utcnow)
+    updated_at = db.Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+    type = db.Column(db.String(50), nullable=False)
+
+    __mapper_args__ = {"polymorphic_identity": "user", "polymorphic_on": type}
 
     def to_json(self):
         return {
             "id": self.id,
-            "user_id": self.user_id,
-            "user": self.user.to_json(),
-            "typeOfService": self.typeOfService,
-            "numberOfReviewsReceived": self.numberOfReviewsReceived,
-            "distance": self.distance,
-            "rating": self.rating
-        }
-
-class JobSeeker(db.Model): 
-    id = db.Column(db.Integer, primary_key=True)
-    numberOfReviewsGiven = db.Column(db.Integer, default=0)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False, unique=True)
-    user = db.relationship('User', backref='job_seeker', uselist=False)
-
-
-    def to_json(self):
-        return {
-            "id" : self.id,
-            "numberOfReviewsGiven": self.numberOfReviewsGiven,
-            "user_id": self.user_id,
-            "user": self.user.to_json(),
-        }
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    firstName = db.Column(db.String(80), unique=False, nullable=False)
-    middleName = db.Column(db.String(80), unique=False, nullable=True)
-    lastName = db.Column(db.String(80), unique=False, nullable=False)
-    email = db.Column(db.String(120), unique=False, nullable=True)
-    houseAddress = db.Column(db.String(225), unique=False, nullable=False)
-    dateOfBirth = db.Column(DateTime, unique=False, nullable=False)
-    phoneNumber = db.Column(db.String(15), unique=False, nullable=False)
-    profilePicture = db.Column(db.String(225), unique=False, nullable=True)
-    gender = db.Column(GENDER_ENUM, unique=False, nullable=False)
-    createdAt = db.Column(DateTime, default=datetime.utcnow)
-    updatedAt = db.Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
-
-    def to_json(self):
-        return {
-            "id" : self.id,
-            "firstName": self.firstName,
-            "middleName": self.middleName,
-            "lastName": self.lastName,
+            "first_name": self.first_name,
+            "middle_name": self.middle_name,
+            "last_name": self.last_name,
             "email": self.email,
-            "houseAddress": self.houseAddress,
-            "dateOfBirth": self.dateOfBirth.isoformat() if self.dateOfBirth else None,
-            "phoneNumber": self.phoneNumber,
-            "profilePicture": self.profilePicture,
+            "house_address": self.house_address,
+            "date_of_birth": (
+                self.date_of_birth.isoformat() if self.date_of_birth else None
+            ),
+            "phone_number": self.phone_number,
+            "profile_picture": self.profile_picture,
             "gender": self.gender,
-            "createdAt": self.createdAt,
-            "updatedAt": self.updatedAt
+            "created_at": self.created_at,
+            "updated_at": self.updated_at,
         }
