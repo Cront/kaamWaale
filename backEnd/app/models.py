f584d7abb89209984eb933cbe2ae1baadfa70450
Hasan <hasan0407@gmail.com>
2025-01-29 20:32:53 -0800

    More backend work, lots left

--- backEnd/app/models.py
+++ backEnd/app/models.py
@@ -17,37 +17,21 @@ SERVICE_TYPE_ENUM = ENUM(
     name='service_type'
 )
 
-
 class ServiceProvider(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-
-    firstName = db.Column(db.String(80), unique=False, nullable=False)
-    middleName = db.Column(db.String(80), unique=False, nullable=True)
-    lastName = db.Column(db.String(80), unique=False, nullable=False)
-    email = db.Column(db.String(120), unique=False, nullable=True)
-    houseAddress = db.Column(db.String(225), unique=False, nullable=False)
-    dateOfBirth = db.Column(DateTime, unique=False, nullable=False)
-    phoneNumber = db.Column(db.String(15), unique=False, nullable=False)
-    profilePicture = db.Column(db.String(225), unique=False, nullable=True)
-    typeOfService = db.Column(SERVICE_TYPE_ENUM, unique=False, nullable=False)
-    gender = db.Column(GENDER_ENUM, unique=False, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False, unique=True)
+    user = db.relationship('User', backref='service_provider', uselist=False)
+    serviceType = db.Column(SERVICE_TYPE_ENUM, unique=False, nullable=False)
     numberOfReviewsReceived = db.Column(db.Integer, default=0)
-    distance = db.Column(db.Integer, default=0.0)
-    rating = db.Column(db.Integer, default=0.0)
+    distance = db.Column(db.Float, default=0.0)
+    rating = db.Column(db.Float, default=0.0)
 
     def to_json(self):
         return {
-            "id" : self.id,
-            "firstName": self.firstName,
-            "middleName": self.middleName,
-            "lastName": self.lastName,
-            "email": self.email,
-            "houseAddress": self.houseAddress,
-            "dateOfBirth": self.dateOfBirth.isoformat() if self.dateOfBirth else None,
-            "phoneNumber": self.phoneNumber,
-            "profilePicture": self.profilePicture,
+            "id": self.id,
+            "user_id": self.user_id,
+            "user": self.user.to_json(),
             "typeOfService": self.typeOfService,
-            "gender": self.gender,
             "numberOfReviewsReceived": self.numberOfReviewsReceived,
             "distance": self.distance,
             "rating": self.rating
@@ -55,7 +39,21 @@ class ServiceProvider(db.Model):
 
 class JobSeeker(db.Model): 
     id = db.Column(db.Integer, primary_key=True)
+    numberOfReviewsGiven = db.Column(db.Integer, default=0)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False, unique=True)
+    user = db.relationship('User', backref='job_seeker', uselist=False)
 
+
+    def to_json(self):
+        return {
+            "id" : self.id,
+            "numberOfReviewsGiven": self.numberOfReviewsGiven,
+            "user_id": self.user_id,
+            "user": self.user.to_json(),
+        }
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
     firstName = db.Column(db.String(80), unique=False, nullable=False)
     middleName = db.Column(db.String(80), unique=False, nullable=True)
     lastName = db.Column(db.String(80), unique=False, nullable=False)
@@ -65,7 +63,6 @@ class JobSeeker(db.Model):
     phoneNumber = db.Column(db.String(15), unique=False, nullable=False)
     profilePicture = db.Column(db.String(225), unique=False, nullable=True)
     gender = db.Column(GENDER_ENUM, unique=False, nullable=False)
-    numberOfReviewsGiven = db.Column(db.Integer, default=0)
     createdAt = db.Column(DateTime, default=datetime.utcnow)
     updatedAt = db.Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
 
@@ -81,7 +78,6 @@ class JobSeeker(db.Model):
             "phoneNumber": self.phoneNumber,
             "profilePicture": self.profilePicture,
             "gender": self.gender,
-            "numberOfReviewsGiven": self.numberOfReviewsGiven,
-            "createdAt": self.createdAt.isoformat() if self.createdAt else None,
-            "updatedAt": self.updatedAt.isoformat() if self.createdAt else None,
+            "createdAt": self.createdAt,
+            "updatedAt": self.updatedAt
         }
