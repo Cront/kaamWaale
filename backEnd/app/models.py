45018a2509a86cf3eeb68b31a924aebe50a114cc
Hasan <hasan0407@gmail.com>
2025-02-02 21:06:02 -0800

    Rearranged files to make backend run

--- backEnd/app/models.py
+++ backEnd/app/models.py
@@ -9,6 +9,41 @@ GENDER_ENUM = ENUM("male", "female", name="gender_type")
 SERVICE_TYPE_ENUM = ENUM("peon", "maid", "driver", name="service_type")
 
 
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(120), nullable=True)
+    house_address = db.Column(db.String(225), nullable=False)
+    date_of_birth = db.Column(DateTime, nullable=False)
+    phone_number = db.Column(db.String(15), nullable=False)
+    profile_picture = db.Column(db.String(225), nullable=True)
+    gender = db.Column(GENDER_ENUM, nullable=False)
+    created_at = db.Column(DateTime, default=datetime.utcnow)
+    updated_at = db.Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+    type = db.Column(db.String(50), nullable=False)
+
+    __mapper_args__ = {"polymorphic_identity": "user", "polymorphic_on": type}
+
+    def to_json(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "email": self.email,
+            "house_address": self.house_address,
+            "date_of_birth": (
+                self.date_of_birth.isoformat() if self.date_of_birth else None
+            ),
+            "phone_number": self.phone_number,
+            "profile_picture": self.profile_picture,
+            "gender": self.gender,
+            "created_at": self.created_at,
+            "updated_at": self.updated_at,
+        }
+
+
 class ServiceProvider(User):
     __tablename__ = "service_providers"
 
@@ -49,38 +84,3 @@ class JobSeeker(User):
         base.update({"service_type": self.service_type})
 
         return base
-
-
-class User(db.Model):
-    __tablename__ = "users"
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), nullable=False)
-    email = db.Column(db.String(120), nullable=True)
-    house_address = db.Column(db.String(225), nullable=False)
-    date_of_birth = db.Column(DateTime, nullable=False)
-    phone_number = db.Column(db.String(15), nullable=False)
-    profile_picture = db.Column(db.String(225), nullable=True)
-    gender = db.Column(GENDER_ENUM, nullable=False)
-    created_at = db.Column(DateTime, default=datetime.utcnow)
-    updated_at = db.Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
-
-    type = db.Column(db.String(50), nullable=False)
-
-    __mapper_args__ = {"polymorphic_identity": "user", "polymorphic_on": type}
-
-    def to_json(self):
-        return {
-            "id": self.id,
-            "name": self.name,
-            "email": self.email,
-            "house_address": self.house_address,
-            "date_of_birth": (
-                self.date_of_birth.isoformat() if self.date_of_birth else None
-            ),
-            "phone_number": self.phone_number,
-            "profile_picture": self.profile_picture,
-            "gender": self.gender,
-            "created_at": self.created_at,
-            "updated_at": self.updated_at,
-        }
