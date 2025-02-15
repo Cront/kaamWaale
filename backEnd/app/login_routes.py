7c553013c9bcb0790aba79c6db3a4098a4675c29
Syed A <63989020+Cront@users.noreply.github.com>
2025-02-15 01:59:15 -0500

    More work on login

--- backEnd/app/login_routes.py
+++ backEnd/app/login_routes.py
@@ -1,20 +1,31 @@
-
 from flask import jsonify, request
-from werkzeug.security import check_password_hash, generate_password_hash
+from werkzeug.security import check_password_hash
 
 from config import app, db
-from models import ServiceProvider
+from models import ServiceProvider, User
 
 
-@app.route("/login", methods=["GET"])
+@app.route("/login", methods=["POST"])
 def login():
-    email = request.json.get("email")
-    password = request.json.get("password").check_password_hash()
-
+    data = request.get_json()
+    email = data.get("email")
+    original_password = data.get("original_password")
 
     if not email:
         return (jsonify({"message": "You must include your email"}))
-    if not password:
+    if not original_password:
         return (jsonify({"message": "You must include your password"}))
 
+    user = User.query.filter_by(email=email).first()
+    if not user:
+        return (jsonify({"message": "User not found"}))
+
+    if not user.check_password_hash(user.password, original_password):
+        return (jsonify({"message": "Incorrect password"}))
+
     return jsonify({"message": "Successfully found email and password"})
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
